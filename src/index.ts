ad9457349c347a5fb6110c9d8d56dc98779570fa
Philipp Bunge <buge@crimson.ch>
2020-09-25 14:46:31 +0200

    Add an index.ts file and compile to es5.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export * as angle from './angle';
+export * as length from './length';
+export * as mass from './mass';
+export * as scalar from './scalar';
+export * as speed from './speed';
+export * as temperature from './temperature';
+export * as time from './time';
+
+export * from './dimension';
+export * from './exponent';
+export * from './unit';
