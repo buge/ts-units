4802928305b66c781cf258f12cb956a38ecc974c
Philipp Bunge <buge@crimson.ch>
2021-08-22 22:16:23 +0200

    Remove redundant exports.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-export * as angle from './angle';
-export * as length from './length';
-export * as mass from './mass';
-export * as scalar from './scalar';
-export * as speed from './speed';
-export * as temperature from './temperature';
-export * as time from './time';
-
 export * from './dimension';
 export * from './exponent';
 export * from './unit';
